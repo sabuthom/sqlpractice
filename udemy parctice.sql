c12a407f7f426e9b68b768248b4a269cd7161a92
sabuthom <sabuvarghesethomas@gmail.com>
2020-02-04 16:33:24 +0530

    Update udemy parctice.sql

--- udemy parctice.sql
+++ udemy parctice.sql
@@ -29,3 +29,34 @@ create table Employees(
     current_status varchar(200) not null default 'employed',
     primary key (id)
 );
+
+INSERT INTO employees(first_name, last_name, age) VALUES
+('Dora', 'Smith', 58);
+ select * from employees;
+ 
+ 
+ DROP TABLE cats;
+ 
+ CREATE TABLE cats 
+  ( 
+     cat_id INT NOT NULL AUTO_INCREMENT, 
+     name   VARCHAR(100), 
+     breed  VARCHAR(100), 
+     age    INT, 
+     PRIMARY KEY (cat_id) 
+  ); 
+  
+  DESC cats;
+  
+  
+INSERT INTO cats(name, breed, age) 
+VALUES ('Ringo', 'Tabby', 4),
+       ('Cindy', 'Maine Coon', 10),
+       ('Dumbledore', 'Maine Coon', 11),
+       ('Egg', 'Persian', 4),
+       ('Misty', 'Tabby', 13),
+       ('George Michael', 'Ragdoll', 9),
+       ('Jackson', 'Sphynx', 7);
+       
+select * from cats;
+  
